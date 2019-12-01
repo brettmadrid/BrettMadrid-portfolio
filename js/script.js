7e0b4df8213f79fbb08098fc951f39a336bd0773
brettmadrid <brettmadrid9@gmail.com>
2019-11-30 17:50:01 -0800

    added owl carousel

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 $(document).ready(function() {
-  $('#slides').superslides({
-    animation: 'fade',
+  $("#slides").superslides({
+    animation: "fade",
     play: 5000,
     pagination: false
   });
 
   const typed = new Typed(".typed", {
-    strings: ["Software Engineer"],
+    strings: ["Software Engineer.", "Team Lead.", "React.", "Node."],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
     showCursor: false
-  })
+  });
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
 });
