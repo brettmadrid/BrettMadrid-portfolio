1dc7a45638c736304ec89658899f7daf86ee304d
brettmadrid <brettmadrid9@gmail.com>
2020-03-26 16:23:09 -0700

    add images with styling to portfolio section

--- js/script.js
+++ js/script.js
@@ -63,4 +63,15 @@ $(document).ready(function() {
       });
     }
   });
+
+  $("[data-fancybox]").fancybox();
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false
+    }
+  });
 });
