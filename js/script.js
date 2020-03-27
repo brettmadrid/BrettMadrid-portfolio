87f44f02f960f03466523d7c7dfe142c5ccd4fe1
brettmadrid <brettmadrid9@gmail.com>
2020-03-26 20:53:02 -0700

    added preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$(window).on("load", function() {
+  $(".loader .inner").fadeOut(500, function() {
+    $(".loader").fadeOut(750);
+  });
+});
+
 $(document).ready(function() {
   $("#slides").superslides({
     animation: "fade",
@@ -124,6 +130,18 @@ $(document).ready(function() {
     return false;
   });
 
+  $("#navigation li a").click(function(e) {
+    e.preventDefault();
+    let targetElement = $(this).attr("href");
+    let targetPosition = $(targetElement).offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: targetPosition - 50
+      },
+      "slow"
+    );
+  });
+
   const nav = $("#navigation");
   const navTop = nav.offset().top;
 
@@ -132,8 +150,10 @@ $(document).ready(function() {
   function stickyNavigation() {
     const body = $("body");
     if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
       body.addClass("fixedNav");
     } else {
+      body.css("padding-top", 0);
       body.removeClass("fixedNav");
     }
   }
